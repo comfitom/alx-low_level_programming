4c6c8267e7f780144b363b31f81c1a6e256d913f
comfitom <comfitbabe@gmail.com>
2022-06-04 04:56:09 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  *  * main - Program exec start here
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     **/
+int main(void)
+{
+		char low;
+
+		for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
+
+		return (0);
+}
