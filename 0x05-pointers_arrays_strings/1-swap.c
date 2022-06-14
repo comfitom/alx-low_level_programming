66edfc14a7494a208645c0b1b1c97eec1c97c5a3
comfitom <comfitbabe@gmail.com>
2022-06-13 17:06:58 -0700

    swapping is easy

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int -> given two integers swap the values they are holding
+ *@a: parameter1
+ *@b: parameter2
+ */
+void swap_int(int *a, int *b);
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
